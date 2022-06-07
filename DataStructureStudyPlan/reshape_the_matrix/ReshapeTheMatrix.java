4c6a902244cab3e6c89f2e41f9fa6b2320cbad37
Christian Juste <chrisfender0@gmail.com>
2022-06-06 17:43:12 -0700

    Reshape the matrix code cleanup

--- DataStructureStudyPlan/reshape_the_matrix/ReshapeTheMatrix.java
+++ DataStructureStudyPlan/reshape_the_matrix/ReshapeTheMatrix.java
@@ -7,23 +7,10 @@ import java.util.List;
  * https://leetcode.com/problems/reshape-the-matrix/
  */
 public class ReshapeTheMatrix {
-	
-	public static void main(String[]args) {
-		int[][] testData = {{1,2},{3,4}};
-		int r = 1;
-		int c = 4;
-		System.out.println(testData[0].length);
-		Utility.printMatrix(testData);
-		Utility.print1DArray(to1DArray(testData));
-		
-	}
 
 	public static int[][] matrixReshape(int[][] mat, int r, int c) {
 		int[] oneDimensionArray = to1DArray(mat);
 		int[][] output = new int[r][c];
-		if(r==1) {
-			return to2DArray(oneDimensionArray);
-		}
 		if(r*c != oneDimensionArray.length) {
 			return mat;
 		}
@@ -51,15 +38,6 @@ public class ReshapeTheMatrix {
 		int[] output = new int[temp.size()];
 		for(int i=0; i<temp.size(); i++) {
 			output[i] = temp.get(i);
-		}
-		return output;
-	}
-	
-	// Special use case when r==1 
-	static int[][] to2DArray(int[] arr) {
-		int[][] output = new int[1][arr.length];
-		for(int i=0; i<arr.length; i++) {
-			output[0][i] = arr[i];
 		}
 		return output;
 	}
